9d7cccf720e25c65d1cd30f0158977425fa1e50e
GautamAnkita <reply.ankita@gmail.com>
2018-06-09 19:00:36 -0700

    Add right and left functions

--- turtle_graphics/turtle.js
+++ turtle_graphics/turtle.js
@@ -2,18 +2,28 @@ class Turtle{
     constructor(x, y){
         this.x = x;
         this.y = y;
-        this.startLoc = {};
-        this.endLoc = {};
-        
-        this.startLoc['xLoc'] = this.x;
-        this.startLoc['yLoc'] = this.y;
-        
-        this.endLoc['xLoc'] = this.x;
-        this.endLoc['yLoc'] = this.y;
+        this.startLoc = [this.x , this.y];
+        this.endLoc = [this.x , this.y];
+        /*
+         0 will point to east, 1 will point to south, 2 will point to west
+         and 3 will point to north as per the right angle directions.
+        */
+        this.currDir = 0; 
     }
 
     forward(steps){
-        this.endLoc['xLoc'] = this.endLoc['xLoc'] + steps;
+        if(this.currDir === 0){
+            this.endLoc[0] = this.endLoc[0] + steps;
+        }
+        else if(this.currDir === 1){
+            this.endLoc[1] = this.endLoc[1] + steps;
+        }
+        else if(this.currDir === 2){
+            this.endLoc[0] = this.endLoc[0] - steps;
+        }
+        else{
+            this.endLoc[1] = this.endLoc[1] - steps;
+        }
         return this;
     }
 
@@ -21,7 +31,27 @@ class Turtle{
         console.log(this.startLoc);
         console.log(this.endLoc);
     }
+
+    right(){
+        if(this.currDir === 3){
+            this.currDir = 0;
+        } else{
+            this.currDir++;
+        }
+        return this;
+    }
+
+    left(){
+        if(this.currDir === 0){
+            this.currDir = 3;
+        } else{
+            this.currDir--;
+        }
+        return this;
+    }
 }
 
-const flash = new Turtle(0,0).forward(3);
-flash.print();
+const flash1 = new Turtle(0, 0).forward(3).right().forward(2);
+flash1.print();
+const flash2 = new Turtle(0, 4).forward(3).left().forward(3);
+flash2.print();
