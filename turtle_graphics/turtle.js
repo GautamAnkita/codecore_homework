f89844ff9e74204ebd23824e5c7258e7ff7cd44b
GautamAnkita <reply.ankita@gmail.com>
2018-06-09 18:00:18 -0700

    Add Forward Function

--- turtle_graphics/turtle.js
+++ turtle_graphics/turtle.js
@@ -2,7 +2,26 @@ class Turtle{
     constructor(x, y){
         this.x = x;
         this.y = y;
+        this.startLoc = {};
+        this.endLoc = {};
+        
+        this.startLoc['xLoc'] = this.x;
+        this.startLoc['yLoc'] = this.y;
+        
+        this.endLoc['xLoc'] = this.x;
+        this.endLoc['yLoc'] = this.y;
+    }
+
+    forward(steps){
+        this.endLoc['xLoc'] = this.endLoc['xLoc'] + steps;
+        return this;
+    }
+
+    print(){
+        console.log(this.startLoc);
+        console.log(this.endLoc);
     }
 }
 
-const flash = new Turtle(0,0);
+const flash = new Turtle(0,0).forward(3);
+flash.print();
