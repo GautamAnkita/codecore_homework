c5b31933be9fcf18d16e01ae60facd377c91e775
GautamAnkita <reply.ankita@gmail.com>
2018-06-09 20:13:45 -0700

    Add print function

--- turtle_graphics/turtle.js
+++ turtle_graphics/turtle.js
@@ -3,6 +3,7 @@ class Turtle{
         this.startLoc = [x , y];
         this.endLoc = [x , y];
         this.allPointsArr = [[x,y]];
+        this.gridSize = (x > y) ? x : y;
 
         /*
          0 will point to east, 1 will point to south, 2 will point to west
@@ -14,25 +15,44 @@ class Turtle{
     forward(steps){
         for(let i=1; i<= steps; i++){
             if(this.currDir === 0){
-                this.endLoc[0] = this.startLoc[0] + i;
+                this.endLoc[0] = this.endLoc[0] + 1;
             }
             else if(this.currDir === 1){
-                this.endLoc[1] = this.startLoc[1] + i;
+                this.endLoc[1] = this.endLoc[1] + 1;
             }
             else if(this.currDir === 2){
-                this.endLoc[0] = this.startLoc[0] - i;
+                this.endLoc[0] = this.endLoc[0] - 1;
             }
             else{
-                this.endLoc[1] = this.startLoc[1] - i;
+                this.endLoc[1] = this.endLoc[1] - 1;
             }
+            this.gridSize = (this.endLoc[0] > this.gridSize) ? this.endLoc[0] : this.gridSize;
+            this.gridSize = (this.endLoc[1] > this.gridSize) ? this.endLoc[1] : this.gridSize;
+
             this.allPointsArr.push([this.endLoc[0], this.endLoc[1]]);
         }
         return this;
     }
 
     print(){
-        console.log(this.startLoc);
-        console.log(this.endLoc);
+        let result = "";
+        for(let i = 0; i <=  this.gridSize; i++ ){
+            for(let j = 0; j <= this.gridSize; j++){
+                if(this.doesLocExist([j,i])) {
+                    result+="#";
+                } else {
+                    result+="-";
+                }
+            }
+            result+="\n";
+        }
+        console.log(result);
+        return this;
+    }
+
+    doesLocExist(loc){
+        return (this.allPointsArr.some(element => 
+            (element[0]=== loc[0] && element[1] === loc[1])));
     }
 
     right(){
@@ -59,6 +79,18 @@ class Turtle{
 
 }
 
-const flash = new Turtle(0, 4).forward(3).left().forward(3);
-console.log(flash.allPoints());
-
+const flash = new Turtle(0, 4)
+  .forward(3)
+  .left()
+  .forward(3)
+  .right()
+  .forward(5)
+  .right()
+  .forward(8)
+  .right()
+  .forward(5)
+  .right()
+  .forward(3)
+  .left()
+  .forward(3)
+  .print();
