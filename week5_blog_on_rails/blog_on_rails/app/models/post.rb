43c3f228477903c5fe446d5819d3e44bc39ba165
GautamAnkita <reply.ankita@gmail.com>
2018-07-03 23:27:19 -0700

    Complete rail_on_blogs app

---
+++ week5_blog_on_rails/blog_on_rails/app/models/post.rb
@@ -0,0 +1,13 @@
+class Post < ApplicationRecord
+    has_many :comments, dependent: :destroy
+
+    validates(:title, presence: true, :uniqueness => {:case_sensitive => false})
+    validates(
+        :body,
+        presence: true,
+        length: {
+            minimum: 2
+        }
+    )
+             
+end
--- week5_blog_on_rails/blog_on_rails/db/seeds.rb
+++ week5_blog_on_rails/blog_on_rails/db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Comment.delete_all
+Post.delete_all
+
+
+500.times do
+    p = Post.create(
+        title: Faker::Job.title,
+        body: Faker::Coffee.notes
+    )
+    if p.valid?
+        rand(0..10).times do
+          Comment.create(
+            body: Faker::Matz.quote,
+            post: p
+          )
+        end
+    end
+end
+
+posts = Post.all
+comments = Comment.all
+
+puts Cowsay.say("Created #{posts.count} posts", :cow)
+puts Cowsay.say("Created #{comments.count} comment", :turkey)
